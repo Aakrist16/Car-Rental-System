407b38f0f0c7d58e338fee39677f86d535950cac
Aakrist16 <aakristacharya47@gmail.com>
2026-06-24 11:31:45 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function Title() {
+  return (
+    <div>
+      <h1>My name is Aakrist </h1>
+      <p>I am your friendly neighbourhood Spiderman</p>
+    </div>
+  );
+}
+export default function App() {
+  return <Title />;
+}
